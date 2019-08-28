9e9bb88ff187dcf04a9bef578d8f61263cc3af38
Diego Banovaz <diego@strive.school>
2019-08-28 18:05:05 +0200

    debrief

--- M5D3/Program.cs
+++ M5D3/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace M5D3
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             IMediaReader mediaReader = new MovieReader();
-            var result = mediaReader.GetByID("tt0241527").Result;
-            var results = mediaReader.Search("Harry%20Potter").Result;
+            var result = await mediaReader.GetByID("tt0241527");
+            var results = await mediaReader.Search("Harry%20Potter");
         }
     }
 }
